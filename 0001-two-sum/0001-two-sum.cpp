ca0376c8e7ffa61cad4aea7b7e40431bacbbfae3
parth134 <86338570+parth134@users.noreply.github.com>
2023-06-21 23:24:32 +0530

    Time: 8 ms (94.33%), Space: 10.9 MB (23.79%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,17 +2,44 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         int n=nums.size();
-        for(int i=0;i<n;i++)
-        {
-            for(int j=i+1;j<n;j++)
-            {
-                if(nums[i]+nums[j]==target)
-                {
-                    return {i,j};
-                }
+        // brute force
+        // for(int i=0;i<n;i++)
+        // {
+        //     for(int j=i+1;j<n;j++)
+        //     {
+        //         if(nums[i]+nums[j]==target)
+        //         {
+        //             return {i,j};
+        //         }
                 
+        //     }
+        // }
+        // return{-1,-1};
+
+        // optimised solution
+        vector<pair<int,int>>v;
+        for(int i=0; i<nums.size();++i){
+            v.push_back(make_pair(nums[i],i));
+        }
+        sort(v.begin(),v.end());
+        int left=0,right=n-1;
+        int sum;
+        while(left<right)
+        {
+             sum=v[left].first+v[right].first;
+            if(sum==target)
+            {
+                return {v[left].second,v[right].second};
+            }
+            else if(sum<target)
+            {
+                left++;
+            }
+            else if(sum>target)
+            {
+                right--;
             }
         }
-        return{-1,-1};
+        return {-1,-1};
     }
 };
