ddc3efe31fc059aed4dc0bfecae4fc7bb82440d1
parth134 <86338570+parth134@users.noreply.github.com>
2023-06-24 21:53:35 +0530

    Time: 92 ms (93.65%), Space: 43.4 MB (94.27%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,53 +1,81 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
+
+
         int n=nums.size();
         if(n==0)
         {
             return 0;
         }
         sort(nums.begin(),nums.end());
-        set<int>result;
-        bool flag = false;
-        int c=0;
-        int max=0;
-       for(int i=0;i<n;i++)
-       {
-           result.insert(nums[i]);
-       }
-       vector<int>output;
-       for(auto it:result)
-       {
-           output.push_back(it);
-       }
-       for(int i=0;i<output.size()-1;i++)
-       {
-           if(output[i]<output[i+1] && output[i+1]-output[i]==1)
-           {
-            c++;
-            flag=true;   
-           }
-           else{
-               if(flag){
-                   if(max<c)
-                   {
-                       max=c;
-                   }
-                c=0;
-                flag=false;
-               }
-           }
-       }
-
-       if(flag){
-
-           if(max<c)
-           {
-               max=c;
-           }
-       }
-
-       return max+1;
-
+        int longestsequence=0;
+        int currentsequence=1;
+        for(int i=1;i<n;i++)
+        {
+            if(nums[i]!=nums[i-1])
+            {
+                if(nums[i]==nums[i-1]+1)
+                {
+                    currentsequence++;
+                }
+                else
+                {
+                    longestsequence=max(longestsequence,currentsequence);
+                    currentsequence=1;
+                }
+    
+        }
+        }
+        return max(longestsequence,currentsequence);
     }
+    //     int n=nums.size();
+    //     if(n==0)
+    //     {
+    //         return 0;
+    //     }
+    //     sort(nums.begin(),nums.end());
+    //     set<int>result;
+    //     bool flag = false;
+    //     int c=0;
+    //     int max=0;
+    //    for(int i=0;i<n;i++)
+    //    {
+    //        result.insert(nums[i]);
+    //    }
+    //    vector<int>output;
+    //    for(auto it:result)
+    //    {
+    //        output.push_back(it);
+    //    }
+    //    for(int i=0;i<output.size()-1;i++)
+    //    {
+    //        if(output[i]<output[i+1] && output[i+1]-output[i]==1)
+    //        {
+    //         c++;
+    //         flag=true;   
+    //        }
+    //        else{
+    //            if(flag){
+    //                if(max<c)
+    //                {
+    //                    max=c;
+    //                }
+    //             c=0;
+    //             flag=false;
+    //            }
+    //        }
+    //    }
+
+    //    if(flag){
+
+    //        if(max<c)
+    //        {
+    //            max=c;
+    //        }
+    //    }
+
+    //    return max+1;
+
+    // }
 };
