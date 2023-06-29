ffc013365e8a9aba7693732f4f6d9b223f857148
parth134 <86338570+parth134@users.noreply.github.com>
2023-06-29 10:10:34 +0530

    Time: 8 ms (71.52%), Space: 14.7 MB (97.44%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -8,42 +8,73 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+// class Solution {
+// public:
+//     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+//         if(list1==NULL)
+//         {
+//             return list2;
+//         }
+//         if(list2==NULL)
+//         {
+//             return list1;
+//         }
+//         ListNode *dummy;
+//         dummy=new ListNode();
+//         ListNode *temp=dummy;
+//         while(list1 && list2)
+//         {
+//             if(list1->val<list2->val)
+//             {
+//                 temp->next=list1;
+//                 list1=list1->next;
+//             }
+//             else
+//             {
+//                 temp->next=list2;
+//                 list2=list2->next;
+//             }
+//             temp=temp->next;
+//         }
+//         if(list1)
+//         {
+//             temp->next=list1;
+//         }
+//         if(list2)
+//         {
+//             temp->next=list2;
+//         }
+//         return dummy->next;
+//     }
+// };
 class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        if(list1==NULL)
-        {
-            return list2;
-        }
-        if(list2==NULL)
-        {
-            return list1;
-        }
-        ListNode *dummy;
-        dummy=new ListNode();
-        ListNode *temp=dummy;
-        while(list1 && list2)
-        {
-            if(list1->val<list2->val)
-            {
-                temp->next=list1;
-                list1=list1->next;
+    ListNode* mergeTwoLists(ListNode* left, ListNode* right) {
+        if(left==NULL)
+            return right;
+        if(right==NULL)
+            return left;
+        ListNode* ans=new ListNode(-1);
+        ListNode* mptr=ans;
+        while(left&&right){
+            if(left->val<=right->val){
+                mptr->next=left;
+                mptr=left;
+                left=left->next;
             }
             else
             {
-                temp->next=list2;
-                list2=list2->next;
+                mptr->next=right;
+                mptr=right;
+                right=right->next;
             }
-            temp=temp->next;
         }
-        if(list1)
-        {
-            temp->next=list1;
+        if(left){
+            mptr->next=left;
         }
-        if(list2)
-        {
-            temp->next=list2;
+        if(right){
+            mptr->next=right;
         }
-        return dummy->next;
+        return ans->next;
     }
 };
