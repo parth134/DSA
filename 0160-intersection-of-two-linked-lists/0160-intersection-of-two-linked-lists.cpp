a26f47b6e2c708353cf7f48fe2f40d5afd1403f0
parth134 <86338570+parth134@users.noreply.github.com>
2023-07-02 19:13:37 +0530

    Time: 40 ms (98.14%), Space: 14.6 MB (60.2%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -11,25 +11,63 @@ public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
 
 
-        unordered_map<ListNode*,int>map;
+        // unordered_map<ListNode*,int>map;
 
-        while(headA || headB){
-            if(headA){
-                if(map[headA]++){
-                    return headA;
-                }
+        // while(headA || headB){
+        //     if(headA){
+        //         if(map[headA]++){
+        //             return headA;
+        //         }
                 
-                headA=headA->next;
-            }
-            if(headB){
-                if(map[headB]++){
-                    return headB;
-                }
-                headB=headB->next;
-            }
+        //         headA=headA->next;
+        //     }
+        //     if(headB){
+        //         if(map[headB]++){
+        //             return headB;
+        //         }
+        //         headB=headB->next;
+        //     }
 
 
+        // }
+        // return NULL;
+        int n=1,m=1;
+        ListNode *p1=headA;
+        ListNode *p2=headB;
+        while(p1!=NULL)
+        {
+            p1=p1->next;
+            n++;
         }
-        return NULL;
+        while(p2!=NULL)
+        {
+            p2=p2->next;
+            m++;
+        }
+        p1=headA;
+        p2=headB;
+        int z=abs(m-n);
+        if(m>n)
+        {
+            while(z)
+            {
+                p2=p2->next;
+                z--;
+            }
+        }
+        else if(n>m)
+        {
+            while(z)
+            {
+                p1=p1->next;
+                z--;
+            }
+        }
+        while(p1!=p2)
+        {
+            p1=p1->next;
+            p2=p2->next;
+        }
+        return p1;
     }
 };
