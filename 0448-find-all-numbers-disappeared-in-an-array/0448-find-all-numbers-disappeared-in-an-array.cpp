4abd506aef40949191510265aed331302e490e5b
parth134 <86338570+parth134@users.noreply.github.com>
2023-10-31 10:41:15 +0530

    Time: 78 ms (31.4%), Space: 33.9 MB (80.38%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
@@ -1,14 +1,38 @@
 class Solution {
 public:
     vector<int> findDisappearedNumbers(vector<int>& nums) {
-        unordered_set<int>s(begin(nums),end(nums));
-        vector<int>ans(size(nums)-size(s));
-        for(int i=1,j=0;i<=size(nums);i++)
-        {
+        // using hashset
+        // unordered_set<int>s(begin(nums),end(nums));
+        // vector<int>ans(size(nums)-size(s));
+        // for(int i=1,j=0;i<=size(nums);i++)
+        // {
            
-            if(!s.count(i))
+        //     if(!s.count(i))
+        //     {
+        //         ans[j++]=i;
+        //     }
+        // }
+        // return ans;
+
+        // brute force
+        // vector<int>ans;
+        // for(int i=1;i<=nums.size();i++)
+        // {
+        //     if(find(nums.begin(),nums.end(),i)==nums.end())
+        //     {
+        //         ans.push_back(i);
+        //     }
+        // }
+        // return ans;
+
+        // sort & binary-search
+        sort(nums.begin(),nums.end());
+        vector<int>ans;
+        for(int i=1;i<=nums.size();i++)
+        {
+            if(!binary_search(nums.begin(),nums.end(),i))
             {
-                ans[j++]=i;
+                ans.push_back(i);
             }
         }
         return ans;
