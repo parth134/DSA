699a6c314844d3704d33cfc5c2c784a0dfc1bf59
parth134 <86338570+parth134@users.noreply.github.com>
2023-11-05 23:37:08 +0530

    Time: 44 ms (77.94%), Space: 48.7 MB (54.96%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int f=0;
+        int c = 0;
+        if(nums.size()==1)
+        return 1;
+        for(int i=nums.size()-2;i>=0;i--){
+            if(nums[i]==0){
+                f=1;
+                
+            }
+            if(f==1){
+                c++;
+                if(nums[i]>=c){
+                    f=0;
+                    c=0;
+                }
+            }
+        }
+       // cout<<c;
+        if (f==1) return 0;
+        return 1;
+    }
+};
+
+// use stack to store values as we itrate 
+// when value zero is reached STOP
+// pop stack and check if a jump is possible over the zero val
+// if possible CLEAR stack and continue 
